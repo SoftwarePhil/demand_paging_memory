5e0bdbd926e1607303d2fb9b3b4fcd321e19fb3f
Phil DiMarco <plantlife26@gmail.com>
2016-10-04 21:08:51 -0400

    final commit

--- src/Test.java
+++ src/Test.java
@@ -6,8 +6,6 @@ public static void main(String[] args){
 	int[] wordRequest = {10, 11, 104, 170, 73, 309, 185, 245, 246, 434, 458, 364};
 					
 	Memory m = new Memory(400, 200, wordRequest);
-	
 	m.runProgram();
-
 }
 }
